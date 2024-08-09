b9d7a1ae1e772e1cd50e8e5fd7c7f13b21a60965
Monica Björk <m.bjork95@gmail.com>
2024-08-09 23:30:47 +0200

    made a category for the devstuff

--- structure/index.ts
+++ structure/index.ts
@@ -1,6 +1,9 @@
 import type { StructureResolver } from "sanity/structure";
 import { CalendarIcon, UsersIcon, PinIcon, DocumentsIcon, HeartIcon, CircleIcon } from "@sanity/icons";
 
+// TODO: Make a GROQ filter for the pages https://www.sanity.io/docs/dynamically-group-list-items-with-a-groq-filter 
+// TODO: Add a image gallery https://www.sanity.io/guides/how-to-use-structured-content-for-page-building
+
 export const structure: StructureResolver = (S) => 
     S.list()
         .id("root")
@@ -10,20 +13,23 @@ export const structure: StructureResolver = (S) =>
             S.documentTypeListItem("location").title("Lokaler").icon(PinIcon),
             S.documentTypeListItem("sponsor").title("Sponsorer").icon(HeartIcon),
             S.divider(),
-            S.divider(),
-            S.listItem()
-                .title("Development zone below").icon('circle'),
-            S.listItem()
-                .title('Upcoming Events')
-                .schemaType('event')
-                .icon(CalendarIcon)
-                .child(S.documentList().title('Upcoming Events').filter('date > now()')),
-            S.listItem()
-                .title('Past Events')
-                .schemaType('event')
-                .icon(CalendarIcon)
-                .child(S.documentList().title('Past Events').filter('date < now()')),
-            S.divider(),
-            S.documentTypeListItem("artist").title("Artists").icon(UsersIcon),
-            S.documentTypeListItem("venue").title("Venues").icon(PinIcon),
+            S.listItem().title("Development").icon('CircleIcon')
+            .child(
+                S.list().title("Dev stuff").items([
+                    S.listItem()
+                        .title('Upcoming Events')
+                        .schemaType('event')
+                        .icon(CalendarIcon)
+                        .child(S.documentList().title('Upcoming Events').filter('date > now()')),
+                    S.listItem()
+                        .title('Past Events')
+                        .schemaType('event')
+                        .icon(CalendarIcon)
+                        .child(S.documentList().title('Past Events').filter('date < now()')),
+                    S.divider(),
+                    S.documentTypeListItem("artist").title("Artists").icon(UsersIcon),
+                    S.documentTypeListItem("venue").title("Venues").icon(PinIcon),
+                ])
+            ),
         ])
+        
